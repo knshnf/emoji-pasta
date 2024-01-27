6daccf3a32abb013c5ecdffeb0ceb0266029300c
kenshin11514 <kenshin_fetalvero@dlsu.edu.ph>
2024-01-28 07:10:17 +0800

    Add text field and button for user input

--- emojipasta.client/src/App.jsx
+++ emojipasta.client/src/App.jsx
@@ -1,17 +1,24 @@
 import './App.css';
+import { useState } from "react";
+
 
 function App() {
-    return (
-        <div>
-            <h1 id="tabelLabel">Emoji Pasta &#x1F35D;</h1>
-            <p> Transform text into lively emoji narratives! &#x1F35D;&#x2728;</p>
-        </div>
-    );
+    const [input, setInput] = useState("");
 
     async function EmojiPasta() {
         const response = await fetch('emojipasta');
         const data = await response.json();
     }
+
+    return (
+        <>
+            <h1 id="tabelLabel"> Emoji &#x1F60A; Pasta&nbsp;&#x1F35D; </h1>
+            <p> Transform &#x1F98B; text  &#x1F48C; into &#x27A1; lively &#x1F917; emoji &#x1FAF6; narratives! &#x2728; </p>
+
+            <textarea onChange={event => setInput(event.target.value)}></textarea>
+            <button> Generate Emoji Pasta </button>
+        </>
+    );
 }
 
 export default App;
